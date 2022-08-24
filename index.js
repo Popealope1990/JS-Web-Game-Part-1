405561d155611d5e539db6ecaa50c23ddf2ad44b
Benjamin Pope <buddypope1234@gmail.com>
2022-08-24 18:53:23 -0500

    commiting changes to code

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(image, left, bottom){
+    const character = document.createElement('img')
+    character.src = image
+    character.style.position = 'fixed'
+    character.style.left = left + 'px'
+    character.style.bottom = bottom + 'px'
+    document.body.append(character)
+    return character
+}
+
+function newItem(image, left, bottom){
+    const newItem = newImage(image, left, bottom)
+    // item.src = item
+    // item.style.position = 'fixed'
+    // item.style.left = left + 'px'
+    // item.style.bottom = bottom + 'px'
+    // document.body.append(item)
+    // return item
+
+    newItem.addEventListener('dblclick', function(){
+        newItem.remove()
+    })
+}
+
+// function backgroundImage(image, bottom){
+//     const backgroundImage = document.createElement('img')
+//     backgroundImage.src = image
+//     backgroundImage.style = 'flex'
+//     backgroundImage.style
+//     backgroundImage.style.bottom = bottom + 'px'
+// }
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+// backgroundImage('assets/sky.png', 400)
